582d543c37fee24aa848c677d8d8b77c8f95a708
Shane Alcock <salcock@waikato.ac.nz>
2020-07-17 21:55:41 -0700

    Update API key check method to return roles for the key owner
    This allows callers to be able to make more fine grained access
    decisions when an API key is used to access protected resources.
    
    Caveats:
     * assumes that no users have the same API key
     * caller must provide the keycloak realm and client as arguments

--- api-key-module/src/main/java/com/gwidgets/resources/ApiKeyResource.java
+++ api-key-module/src/main/java/com/gwidgets/resources/ApiKeyResource.java
@@ -2,12 +2,19 @@ package com.gwidgets.resources;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Iterator;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserModel;
+import org.keycloak.models.RoleModel;
+import org.keycloak.models.ClientModel;
+import com.google.gson.Gson;
 
 public class ApiKeyResource {
 
@@ -23,8 +30,27 @@ public class ApiKeyResource {
 
     @GET
     @Produces("application/json")
-    public Response checkApiKey(@QueryParam("apiKey") String apiKey) {
-        List<UserModel> result = session.userStorageManager().searchForUserByUserAttribute("api-key", apiKey, session.realms().getRealm(realmName));
-        return result.isEmpty() ? Response.status(401).build(): Response.ok().build();
+    public Response checkApiKey(@QueryParam("apiKey") String apiKey,
+                @QueryParam("realm") String realmname,
+                @QueryParam("client") String clientname) {
+        List<UserModel> result = session.userStorageManager().searchForUserByUserAttribute("api-key", apiKey, session.realms().getRealm(realmname));
+
+        if (result.isEmpty()) {
+                return Response.status(401).build();
+        }
+
+        ClientModel client = session.realms().getRealm(realmname).getClientByClientId(clientname);
+        ArrayList<String> l = new ArrayList<String>();
+        if (client != null) {
+            // hopefully no clashes ...
+            Set<RoleModel> roles = result.get(0).getClientRoleMappings(client);
+            Iterator<RoleModel> setit = roles.iterator();
+            while (setit.hasNext()) {
+                l.add(setit.next().getName());
+            }
+        }
+
+        String json = new Gson().toJson(l);
+        return Response.ok(json).build();
     }
 }
